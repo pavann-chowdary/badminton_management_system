e0709c84e39bc0a902b12c4d3af9e14fb6dff0b0
Pavann Chowdary <pavannchowdary1224@gmail.com>
2025-03-18 03:40:13 -0500

    imported routes

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -3,13 +3,13 @@ import {useLocation} from 'react-router-dom'
 
 const Profile = () => {
     const location = useLocation();
-    const { given_name, email } = location.state || {}; //if location.state is undefined, it becomes an empty object.
+    const { name, email } = location.state || {}; //if location.state is undefined, it becomes an empty object.
     
   return (
     <div>
         Profile
         <h1>
-            {given_name}
+            {name}
         </h1>
         <h1>
             {email}
