1ce6c6e4867c2f5edae779c50f188cf0f06f0faf
Pavann Chowdary <pavannchowdary1224@gmail.com>
2025-03-18 01:06:22 -0500

    changed mongo password

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 const config = {
-    MONGODB_URI: 'mongodb+srv://pavannchowdary:Mnb_1290@cluster0.aiw8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Replace with your actual URI
+    MONGODB_URI: 'mongodb+srv://pavannchowdary:bobby@cluster0.aiw8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Replace with your actual URI
   };
 
 export default config;
