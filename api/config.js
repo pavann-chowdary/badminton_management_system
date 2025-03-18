e65f546b3f0b0fa7d21a068225f303a549ce322b
Pavann Chowdary <pavannchowdary1224@gmail.com>
2025-03-18 00:24:04 -0500

    modified to export default

--- api/config.js
+++ api/config.js
@@ -1,3 +1,5 @@
-export default config = {
+const config = {
     MONGODB_URI: 'mongodb+srv://pavannchowdary:Mnb_1290@cluster0.aiw8g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', // Replace with your actual URI
   };
+
+export default config;
